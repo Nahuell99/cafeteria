7f9bf487610d1f12a5f409cb1a927f8bc091f513
Nahuel <Naahuelll99@gmail.com>
2022-07-12 17:40:13 -0300

    Formulario 1

--- src/app/sidebar/sidebar.module.ts
+++ src/app/sidebar/sidebar.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { RouterModule } from '@angular/router';
+import { FormularioComponent } from './formulario/formulario.component';
 
 
 
 @NgModule({
   declarations: [
-    MenuComponent
+    MenuComponent,
+    FormularioComponent
   ],
   imports: [
     CommonModule,
