4ce19535f437b5fc0ffe0f2abcc81c022b1a5f5d
Nahuel <Naahuelll99@gmail.com>
2022-07-12 01:11:37 -0300

    Prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarModule } from './sidebar/sidebar.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SidebarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
